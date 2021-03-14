1552376d932fca438d0e9e57e5bd088704b66a6f
Iudin_Aleksei <aviordream@gmail.com>
2021-03-14 13:06:02 +0300

    feat: add configs

--- react-hooks-cc/src/App.js
+++ react-hooks-cc/src/App.js
@@ -1,7 +1,44 @@
+import React, { useState } from 'react'
+
+function computeInitialCounter() {
+  return Math.trunc(Math.random() * 20);
+}
 
 function App() {
+  const [counter, setCounter] = useState(() => {
+    return computeInitialCounter()
+  });
+
+  const [state, setState] = useState({
+    title: 'Счетчик',
+    date: Date.now(),
+  });
+
+  function increment() {
+    setCounter((prev) => prev + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
+
+  function updateTitle() {
+    setState(prev => {
+      return {
+        ...prev,
+        title: 'New title'
+      }
+    });
+  }
+
   return (
-    <div>      
+    <div>
+      <h1>Счетчик: {counter}</h1>
+      <button onClick={increment} className="btn btn-success">Добавить</button>
+      <button onClick={decrement} className="btn btn-danger">Убрать</button>
+      <button onClick={updateTitle} className="btn btn-default">Изменить состояние</button>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
